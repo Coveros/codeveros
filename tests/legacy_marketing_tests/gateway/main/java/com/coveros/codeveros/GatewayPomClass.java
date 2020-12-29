e8227cda51499b3334c6081a21baa32ece457ac5
Justin Hollingsworth <justin.hollingsworth@coveros.com>
2020-12-29 16:38:00 -0500

    add legacy test files from marketing demo branches of gitlab repos
    Signed-off-by: Justin Hollingsworth <justin.hollingsworth@coveros.com>

---
+++ tests/legacy_marketing_tests/gateway/main/java/com/coveros/codeveros/GatewayPomClass.java
@@ -0,0 +1,7 @@
+package com.coveros.codeveros;
+
+public class GatewayPomClass {
+
+    //POM Class description
+
+}
---
+++ tests/legacy_marketing_tests/ui/main/java/com/coveros/codeveros/AngularUIPomClass.java
@@ -0,0 +1,7 @@
+package com.coveros.codeveros;
+
+public class AngularUIPomClass {
+
+  //POM Class description
+
+}
