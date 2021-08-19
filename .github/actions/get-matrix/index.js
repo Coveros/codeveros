0dbb9897b91694cb4c2e9c25631272ebd1f1431c
Justin Hollingsworth <justin.hollingsworth@coveros.com>
2021-08-19 16:13:14 -0400

    try out retrieving matrix values from action

---
+++ .github/actions/get-matrix/index.js
@@ -0,0 +1,37 @@
+const core = require('@actions/core');
+
+const serviceValues = {
+  auth: {
+    context: 'services/auth-service/nodejs',
+    image: 'coveros/codeveros-auth-service'
+  },
+  gateway: {
+    context: 'services/gateway/nodejs',
+    image: 'coveros/codeveros-gateway'
+  },
+  training: {
+    context: 'services/training-service/nodejs',
+    image: 'coveros/codeveros-training-service'
+  },
+  user: {
+    context: 'services/user-service/nodejs',
+    image: 'coveros/codeveros-user-service'
+  },
+  ui: {
+    context: 'services/ui/angular',
+    image: 'coveros/codeveros-ui'
+  }
+};
+
+try {
+  const services = JSON.parse(core.getInput('services'));
+  let include = [];
+  for (let service of services) {
+    if (serviceValues.hasOwnProperty(service)) {
+      include.push(serviceValues[service])
+    }
+  }
+  core.setOutput('matrix', JSON.stringify({ include }));
+} catch (error) {
+  core.setFailed(error.message);
+}
