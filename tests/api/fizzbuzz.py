b9d879df8661cb54d4e33a765516f79a640311a0
Matthew <matthew.taylor@coveros.com>
2021-10-01 17:01:27 -0400

    Correct api endpoint

--- tests/api/fizzbuzz.py
+++ tests/api/fizzbuzz.py
@@ -2,5 +2,5 @@ import requests, sys
 
 appUrl = sys.argv[1]
 
-r = requests.get(url=f"${appUrl}/fizzbuzz", params="size=10")
+r = requests.get(url=f"${appUrl}api/fizzbuzz", params="size=10")
 assert(r.text == '[1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz"]')
