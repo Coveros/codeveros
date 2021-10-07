b66215415dd4b92e5fd9a5f3435f2b75a26430c2
Matthew <matthew.taylor@coveros.com>
2021-10-07 17:49:54 -0400

    Organize test

--- tests/api/fizzbuzz.py
+++ tests/api/fizzbuzz.py
@@ -2,5 +2,6 @@ import requests, sys
 
 appUrl = sys.argv[1]
 
-r = requests.get(url=f"{appUrl}api/fizzbuzz", params="size=10")
-assert(r.text == '[1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz"]')
+actual = requests.get(url=f"{appUrl}api/fizzbuzz", params="size=10").text
+expected = '[1,2,"Fizz",4,"Buzz","Fizz",7,8,"Fizz","Buzz"]'
+assert(actual == expected)
