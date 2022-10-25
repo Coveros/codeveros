9e6b2fd929b86582e41e58cd433af5eaa7102bce
Manjusha Padmanabhan <MPadmanabhan@mercuryinsurance.com>
2022-10-25 09:45:44 -0700

    Added checkout and new stage lint

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,5 +1,13 @@
 node {
+    checkout
     stage('Build') {
          echo 'Hello World'          
+    }
+    stage('lint') {
+        try {
+                echo 'linting'   
+        } catch (Exception e) {
+                    echo 'Failed linting' + e.toString()
+        }
     }
 }
