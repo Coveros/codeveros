33f2e273153784ee81beeb99050d07c5c532d3e6
Manjusha Padmanabhan <MPadmanabhan@mercuryinsurance.com>
2022-10-25 10:17:23 -0700

    Updated  sh 'npm ci --quiet --cache="./npm"'

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,12 +1,12 @@
 node {
     checkout scm
     dir(‘services/ui/angular’) {
-        stage('Dependencies') {
-            docker.image(‘node:14.16’).inside{
-                sh 'npm ci --quiet --cache="./npm"'
-            }           
-        }
-            stage('Build') {
+      stage('Dependencies') {
+      docker.image('node:14.16').inside {
+        sh 'npm ci --quiet --cache="./npm"'
+      }
+    }
+    stage('Build') {
                 echo 'Hello World'          
             }
             stage('lint') {
