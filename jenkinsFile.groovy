f12bbdcf46826551acdc1171026cba8349d18c95
rherrera <rherrera@mercuryinsurance.com>
2022-10-25 09:33:49 -0700

    Update jenkinsFile
    Add checkout step to pipeline stage build
    Add a lint stage
    Add a try catch block to lint stage

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -2,4 +2,14 @@ node {}
 
 stage('build') {
     echo 'Sup'
+
+    checkout([$class: 'GitSCM', branches: [[name: '*/master']], extensions: [], userRemoteConfigs: [[credentialsId: 'rawrool1', url: 'https://github.com/rawrool/codeveros.git']]])
+}
+
+stage('lint') {
+    try {
+        echo 'linting'
+    } catch (Exception e) {
+        echo 'Failed linting' + e.toString()
+    }
 }
