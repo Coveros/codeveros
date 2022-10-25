d3345ecfd536e944d3dcb823be6ec4abdc44c697
Manjusha Padmanabhan <MPadmanabhan@mercuryinsurance.com>
2022-10-25 10:19:19 -0700

    Updated code to fix errors

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,19 +1,21 @@
 node {
     checkout scm
-    dir(‘services/ui/angular’) {
-      stage('Dependencies') {
-      docker.image('node:14.16').inside {
-        sh 'npm ci --quiet --cache="./npm"'
-      }
-    }
-    stage('Build') {
-                echo 'Hello World'          
+    dir('services/ui/angular'){
+        stage('Dependencies'){
+            docker.image('node:14.16').inside { //creates node in docker then go inside
+                //run these
+                sh 'npm ci --quiet --cache="./npm"'  //copies the user, mounts the directories, maps it to the exact same location as node agent, gives permissions\
+                //define location of cache files to be in the workspace.
             }
-            stage('lint') {
-                try {
-                   echo 'linting'   
-                } catch (Exception e) {
-                    echo 'Failed linting' + e.toString()
+        }
+        stage('Build') {
+            echo 'Hello World'
+        }
+        stage('lint') {
+            try{
+                echo 'Linting'
+            }catch(Exception e) {
+                echo 'Failed linting ' + e.toString()
             }
         }
     }
