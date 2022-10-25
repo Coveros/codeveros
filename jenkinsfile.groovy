11f52e28f890e675ca6e1b1e450f12e5560c8fed
Manjusha Padmanabhan <MPadmanabhan@mercuryinsurance.com>
2022-10-25 10:22:52 -0700

    added checkout

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,22 +1,29 @@
 node {
-    checkout scm
-    dir('services/ui/angular'){
-        stage('Dependencies'){
+        stage('cleanup'){
+            cleanWS()
+        }
+        checkout scm
+        dir('services/ui/angular'){
+            stage('Dependencies'){
+                docker.image('node:14.16').inside { //creates node in docker then go inside
+                    //run these
+                    sh 'npm ci --quiet --cache="./npm"'  //copies the user, mounts the directories, maps it to the exact same location as node agent, gives permissions\
+                    //define location of cache files to be in the workspace.
+                }
+            }
+            stage('Build') {
             docker.image('node:14.16').inside { //creates node in docker then go inside
-                //run these
-                sh 'npm ci --quiet --cache="./npm"'  //copies the user, mounts the directories, maps it to the exact same location as node agent, gives permissions\
-                //define location of cache files to be in the workspace.
+                    //run these
+                    sh 'npm run build.production --cache="./npm"'  //copies the user, mounts the directories, maps it to the exact same location as node agent, gives permissions\
+                    //define location of cache files to be in the workspace.
+                }
+            }
+            stage('lint') {
+                try{
+                    echo 'Linting'
+                }catch(Exception e) {
+                    echo 'Failed linting ' + e.toString()
+                }
             }
         }
-        stage('Build') {
-            echo 'Hello World'
-        }
-        stage('lint') {
-            try{
-                echo 'Linting'
-            }catch(Exception e) {
-                echo 'Failed linting ' + e.toString()
-            }
-        }
-    }
 }
