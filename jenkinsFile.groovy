e24063b3aa55d8078440337c4e11d48770d87f69
rherrera <rherrera@mercuryinsurance.com>
2022-10-25 10:13:36 -0700

    Update jenkinsFile
    Add dependencies stage

--- jenkinsFile.groovy
+++ jenkinsFile.groovy
@@ -1,15 +1,24 @@
 node {
     checkout scm
-    
-    stage('build') {
-        echo 'Sup'
-    }
 
-    stage('lint') {
-        try {
-            echo 'linting'
-        } catch (Exception e) {
-            echo 'Failed linting' + e.toString()
+    dir('services/ui/angular') {
+        stage('dependencies') {
+            docker.image('node:14.16').inside {
+                sh 'npm ci --quiet --cache="./npm"'
+            }
+        }
+        
+        stage('build') {
+            echo 'Sup'
+        }
+
+        stage('lint') {
+            try {
+                echo 'linting'
+            } catch (Exception e) {
+                echo 'Failed linting' + e.toString()
+            }
         }
     }
+    
 }
