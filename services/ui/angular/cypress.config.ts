846c57dfcf8b4be1472c38a303cfdc987edec652
Justin Hollingsworth <jwhollingsworth@gmail.com>
2025-08-04 09:28:16 -0400

    remove example files

--- services/ui/angular/cypress.config.ts
+++ services/ui/angular/cypress.config.ts
@@ -1,18 +1,17 @@
-import { defineConfig } from 'cypress'
+import { defineConfig } from 'cypress';
 
 export default defineConfig({
-  
   e2e: {
-    'baseUrl': 'http://localhost:4200'
+    baseUrl: 'http://localhost:4200',
+    supportFile: false,
   },
-  
-  
+
   component: {
     devServer: {
       framework: 'angular',
       bundler: 'webpack',
     },
-    specPattern: '**/*.cy.ts'
-  }
-  
-})
+    specPattern: '**/*.cy.ts',
+    supportFile: false,
+  },
+});
